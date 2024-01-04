1223bbe8214aa0121ac14d156a7f9f2586ef8beb
Blaine Curren <blaine.curren@gmail.com>
2024-01-03 19:23:08 -0600

    added handlebars middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
@@ -14,6 +15,9 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+// Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 const PORT = process.env.PORT || 5000;
 
